426406c737634f77deffefd6288f817ee095687d
myari211 <apputra16@gmail.com>
2021-12-21 23:37:09 +0700

    add icon title bar

--- resources/views/master/css.blade.php
+++ resources/views/master/css.blade.php
@@ -5,6 +5,7 @@
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 <title>Body Gravity</title>
+<link rel="icon" href="{{ asset('favicon.ico') }}">
 <style type="text/css">
     html, body{
         font-family: 'Montserrat', sans-serif;
