424ea6a8ec26527bf535cac7f0789ac3940b23db
myari211 <apputra16@gmail.com>
2021-12-30 10:52:25 +0700

    repair appServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -14,6 +14,7 @@ class AppServiceProvider extends ServiceProvider
     public function register()
     {
         RealRashid\SweetAlert\SweetAlertServiceProvider::class;
+        Schema::defaultStringLength(191);
     }
 
     /**
